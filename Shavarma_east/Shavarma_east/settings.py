b73689e6abfeab47bf6ae316e2cd15270609e302
Artem <Popovartem3412@gmail.com>
2023-04-12 16:35:07 +0300

    models.py in app and setting in main project

--- Shavarma_east/Shavarma_east/settings.py
+++ Shavarma_east/Shavarma_east/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Restaurant.apps.RestaurantConfig',
     'Restaurant',
     'django.contrib.admin',
     'django.contrib.auth',
