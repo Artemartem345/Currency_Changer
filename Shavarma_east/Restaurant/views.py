4454db2cb51078530aacb12d4a16f4af99cb802c
Artem <Popovartem3412@gmail.com>
2023-04-18 19:24:07 +0300

    Чекни урлс и виевс py примерная логика

--- Shavarma_east/Restaurant/views.py
+++ Shavarma_east/Restaurant/views.py
@@ -3,4 +3,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def func(request):
-    return HttpResponse('Hello there !!!adsadasd')
+    return HttpResponse('Hello there its best shavarma in the world!')
+
+
+
+def func1(request):
+    return HttpResponse('Hello its a lot of drinks !')
+
+
+
+def func2(request):
+    return HttpResponse('Hello there is a lot of pizzas!')
+
+
+
+
+
