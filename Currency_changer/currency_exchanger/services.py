794d9537408eab36a9b9b0793bcdfff77b50ba3d
Artem <Popovartem3412@gmail.com>
2023-05-08 16:42:18 +0300

    some_adds

---
+++ Currency_changer/currency_exchanger/services.py
@@ -0,0 +1,6 @@
+
+from .models import CurrencyRate, Currency
+
+def calculate(currency:str, amount:int):
+    return Currency.objects.filter()
+    
--- Currency_changer/currency_exchanger/views.py
+++ Currency_changer/currency_exchanger/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 import requests
 from .models import CurrencyRate, Currency
 from .serializer import CurrencyRateSerializer, CurrencySerializer
-
+from .services import calculate
 
 class CurrencyAPIView(APIView):
     def get(self, request, *args, **kwargs):
@@ -16,17 +16,18 @@ class CurrencyAPIView(APIView):
         print(query_data)
         api_route = 'https://openexchangerates.org/api/latest.json?app_id=f39eeff3540a41fa919debe87b0071de'
         data = requests.get(api_route)
-        new_data = data.json()
+        _data = data.json()
         # Dollar data
-        data_dollar = new_data['rates'][query_data]
+        currency_data = _data['rates'][query_data]
         # CurrencyRate.objects.create()
         # Currency django admin
         cur = Currency.objects.get(name=query_data)  
-        currency_rate = CurrencyRate.objects.create(currency=cur, course=data_dollar)
+        currency_rate = CurrencyRate.objects.create(currency=cur, course=currency_data)
         currency_rate.save()
         serializer = CurrencyRateSerializer(currency_rate)
         # if serializer.is_valid():
         #     serializer.save()
+        print(calculate('USD', '20'))
         return Response(data=serializer.data, status=status.HTTP_200_OK)
     
         
