a717414dd24291bc9bb3594869356e3af7bd90f9
Artem <Popovartem3412@gmail.com>
2023-06-18 18:49:52 +0300

    edits

--- Currency_changer/Currency_changer/settings.py
+++ Currency_changer/Currency_changer/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)}
                                                                   
 SIMPLE_JWT = {
